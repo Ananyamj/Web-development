452fd2e82db3db7aa6d49217faf4950b3331f077
Ananyamj <162456526+Ananyamj@users.noreply.github.com>
2024-06-15 11:13:01 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from './image.png';
+import './About.css'
+function About() {
+  return (
+    <>
+      <img src={Image} alt="" className='image' />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illum est odio quos non numquam a quibusdam? Ullam consequatur, veritatis eveniet, voluptatibus eligendi blanditiis, aliquam deserunt corrupti laboriosam a soluta!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto molestias provident totam quidem deleniti necessitatibus ullam, odit praesentium placeat obcaecati, minus voluptatem ratione adipisci reiciendis dignissimos. Laboriosam nisi quibusdam enim.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas laudantium possimus voluptas quod tenetur maxime id perspiciatis error alias ex esse aperiam autem quo vero, earum necessitatibus sequi sapiente quia.</p>
+      </>
+  )
+}
+
+export default About
---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1 style={{letterSpacing:"2px"}}>SOFTWARE DEVELOPER</h1>
+    </div>
+  )
+}
+
+export default Header
